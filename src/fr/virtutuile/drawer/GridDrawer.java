d41aadd5cb13b43dc17392753a257e888de23116
Capt.KAS <antoine.casse@epitech.eu>
2019-11-23 23:23:41 +0100

    Dotted grid

--- src/fr/virtutuile/drawer/GridDrawer.java
+++ src/fr/virtutuile/drawer/GridDrawer.java
@@ -14,17 +14,22 @@ public class GridDrawer {
         width = w;
     }
 
+    public void drawDashedLine(Graphics2D g2, Line2D line) {
+        Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+        g2.setStroke(dashed);
+        g2.draw(line);
+    }
+
     public void draw(Graphics g) {
-        //System.out.printLn("helloprint");
         Graphics2D g2 = (Graphics2D) g;
 
         for (int i = 100 ; i < width ; i += 100) {
             Line2D line = new Line2D.Float(i, 0, i, height);
-            g2.draw(line);
+            drawDashedLine(g2, line);
         }
         for (int i = 100 ; i < height ; i += 100) {
             Line2D line = new Line2D.Float(0, i, width, i);
-            g2.draw(line);
+            drawDashedLine(g2, line);
         }
     }
 }
