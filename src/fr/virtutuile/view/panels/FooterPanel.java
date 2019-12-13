2d259b199cc321751326ca90ed6c11a242888ed9
Zakaria LAABID <zakaria.laabid@epitech.eu>
2019-12-13 16:19:08 -0500

    fix: correcting bad import

--- src/fr/virtutuile/view/panels/FooterPanel.java
+++ src/fr/virtutuile/view/panels/FooterPanel.java
@@ -1,7 +1,5 @@
 package fr.virtutuile.view.panels;
 
-import fr.virtutuile.domain.Material;
-import fr.virtutuile.domain.Surface;
 import fr.virtutuile.domain.ZoomControllerObserver;
 import fr.virtutuile.drawer.GridDrawer;
 import fr.virtutuile.view.frames.MainWindow;
@@ -11,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import net.miginfocom.swing.MigLayout;
 
 public class FooterPanel extends JPanel implements ZoomControllerObserver {
 
@@ -27,22 +24,20 @@ public class FooterPanel extends JPanel implements ZoomControllerObserver {
 		this.gridDrawer = gridDrawer;
 		buildUp();
 		mainWindow.controller.registerObserverZoom(this);
+	}
 
-		JLabel labelGrid = new JLabel("Taille de la grille :");
-		add(labelGrid, "cell 0 1");
-
-		textFieldGrid = new JTextField("valeur par default a modifié");
-		textFieldGrid.setColumns(10);
-		textFieldGrid.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("here mettre le changement de size de la grid " + Integer.parseInt(textFieldGrid.getText()));
-			}
-		});
-		add(textFieldGrid, "cell 1 1,growx");
-
-		labelSurfaceNumber = new JLabel("Taille  de detection");
-		add(labelSurfaceNumber, "cell 4 1");
+	private void buildUp() {
+		Dimension footerDimension = this.getPreferredSize();
+		footerDimension.height = 45;
+		setPreferredSize(new Dimension(689, 59));
+		setBackground(Color.white);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 121, 110, 41, 130, 110, 0 };
+		gridBagLayout.rowHeights = new int[] { 15, 19, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
 
 		textFieldDetect = new JTextField();
 		textFieldDetect.addActionListener(new ActionListener() {
@@ -52,19 +47,53 @@ public class FooterPanel extends JPanel implements ZoomControllerObserver {
 
 			}
 		});
-		add(textFieldDetect, "cell 6 1,growx");
-		textFieldDetect.setColumns(10);
-	}
-
-	private void buildUp() {
-		Dimension footerDimension = this.getPreferredSize();
-		footerDimension.height = 45;
-		setPreferredSize(new Dimension(689, 59));
-		setBackground(Color.white);
-		setBorder(BorderFactory.createLineBorder(Color.black));
-		setLayout(new MigLayout("", "[8px][][][][][][grow][124.00,grow][]", "[8px][]"));
 		labelZoom = new JLabel("Zoom : " + (int) (mainWindow.controller.getZoom() * 100) + "%");
-		add(labelZoom, "cell 0 0,grow");
+		GridBagConstraints gbc_labelZoom = new GridBagConstraints();
+		gbc_labelZoom.fill = GridBagConstraints.BOTH;
+		gbc_labelZoom.insets = new Insets(0, 0, 5, 5);
+		gbc_labelZoom.gridx = 0;
+		gbc_labelZoom.gridy = 0;
+		add(labelZoom, gbc_labelZoom);
+
+		textFieldGrid = new JTextField("valeur par default a modifié");
+		textFieldGrid.setColumns(10);
+		textFieldGrid.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(
+						"here mettre le changement de size de la grid " + Integer.parseInt(textFieldGrid.getText()));
+			}
+		});
+
+		JLabel labelGrid = new JLabel("Taille de la grille :");
+		GridBagConstraints gbc_labelGrid = new GridBagConstraints();
+		gbc_labelGrid.anchor = GridBagConstraints.WEST;
+		gbc_labelGrid.insets = new Insets(0, 0, 0, 5);
+		gbc_labelGrid.gridx = 0;
+		gbc_labelGrid.gridy = 1;
+		add(labelGrid, gbc_labelGrid);
+		GridBagConstraints gbc_textFieldGrid = new GridBagConstraints();
+		gbc_textFieldGrid.anchor = GridBagConstraints.NORTH;
+		gbc_textFieldGrid.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldGrid.insets = new Insets(0, 0, 0, 5);
+		gbc_textFieldGrid.gridx = 1;
+		gbc_textFieldGrid.gridy = 1;
+		add(textFieldGrid, gbc_textFieldGrid);
+
+		labelSurfaceNumber = new JLabel("Taille  de detection");
+		GridBagConstraints gbc_labelSurfaceNumber = new GridBagConstraints();
+		gbc_labelSurfaceNumber.anchor = GridBagConstraints.WEST;
+		gbc_labelSurfaceNumber.insets = new Insets(0, 0, 0, 5);
+		gbc_labelSurfaceNumber.gridx = 3;
+		gbc_labelSurfaceNumber.gridy = 1;
+		add(labelSurfaceNumber, gbc_labelSurfaceNumber);
+		GridBagConstraints gbc_textFieldDetect = new GridBagConstraints();
+		gbc_textFieldDetect.anchor = GridBagConstraints.NORTH;
+		gbc_textFieldDetect.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldDetect.gridx = 4;
+		gbc_textFieldDetect.gridy = 1;
+		add(textFieldDetect, gbc_textFieldDetect);
+		textFieldDetect.setColumns(10);
 	}
 
 	@Override
