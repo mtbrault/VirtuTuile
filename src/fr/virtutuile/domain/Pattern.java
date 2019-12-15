26c49a950f0bd78a46a35f15b5e4482ed544a69d
valerian huylebroeck <valerian.huylebroeck@epitech.eu>
2019-12-15 12:52:44 -0500

    add new rotate motif

--- src/fr/virtutuile/domain/Pattern.java
+++ src/fr/virtutuile/domain/Pattern.java
@@ -162,7 +162,7 @@ public class Pattern implements java.io.Serializable {
                     }
                 }
             }
-        } else if (surface.getPatternId() == 1) {
+        } else if (surface.getPatternId() == 2) {
             for (int y = 0; y < nbYTiles / 2 + 1; y++) {
                 for (int x = 0; x < nbXTiles; x++) {
                     ArrayList points = new ArrayList<Point>();
@@ -206,6 +206,77 @@ public class Pattern implements java.io.Serializable {
                     Tile tile2 = this.getIntersection(surface, new Tile(secondPoints));
                     if (tile2 != null) {
                         tiles.add(tile2);
+                    }
+                }
+            }
+        } else if (surface.getPatternId() == 1) {
+            for (int y = 0; y < nbYTiles; y++) {
+                for (int x = 0; x < nbXTiles; x++) {
+                    ArrayList points = new ArrayList<Point>();
+                    Point center = new Point(minPointX + x * tileWidth + (tileWidth / 2) + (surface.getJointSize() * x), minPointY + y * tileHeight + (tileHeight / 2) + (y*surface.getJointSize()));
+                    int diagonalSize = (int)Math.sqrt(Math.pow(tileWidth, 2) + Math.pow(tileHeight, 2));
+                    int Xpoint0 = x * tileWidth + minPointX + (surface.getJointSize() * x) - center.x;
+                    int Ypoint0 = y * tileHeight + minPointY + (y*surface.getJointSize())  - center.y;
+                    int Xpoint1 = (x + 1) * tileWidth + minPointX + (surface.getJointSize() * x) - center.x;
+                    int Ypoint1 = y * tileHeight + minPointY + (y*surface.getJointSize())  - center.y;
+                    int Xpoint2 = (x + 1) * tileWidth + minPointX + (x*surface.getJointSize()) - center.x;
+                    int Ypoint2 = (y + 1) * tileHeight + minPointY + (y*surface.getJointSize()) - center.y;
+                    int Xpoint3 = x * tileWidth + minPointX + (x*surface.getJointSize()) - center.x;
+                    int Ypoint3 = (y + 1) * tileHeight + minPointY + (y*surface.getJointSize()) - center.y;
+                    int angle = -45;
+                    int pointx0 = (int)(Xpoint0 * Math.cos(Math.toRadians(angle)) - Ypoint0 * Math.sin(Math.toRadians(angle)));
+                    int pointy0 = (int)(Xpoint0 * Math.sin(Math.toRadians(angle)) + Ypoint0 * Math.cos(Math.toRadians(angle)));
+                    int pointx1 = (int)(Xpoint1 * Math.cos(Math.toRadians(angle)) - Ypoint1 * Math.sin(Math.toRadians(angle)));
+                    int pointy1 = (int)(Xpoint1 * Math.sin(Math.toRadians(angle)) + Ypoint1 * Math.cos(Math.toRadians(angle)));
+                    int pointx2 = (int)(Xpoint2 * Math.cos(Math.toRadians(angle)) - Ypoint2 * Math.sin(Math.toRadians(angle)));
+                    int pointy2 = (int)(Xpoint2 * Math.sin(Math.toRadians(angle)) + Ypoint2 * Math.cos(Math.toRadians(angle)));
+                    int pointx3 = (int)(Xpoint3 * Math.cos(Math.toRadians(angle)) - Ypoint3 * Math.sin(Math.toRadians(angle)));
+                    int pointy3 = (int)(Xpoint3 * Math.sin(Math.toRadians(angle)) + Ypoint3 * Math.cos(Math.toRadians(angle)));
+                    int shiftX =  (diagonalSize - tileWidth) * x;
+                    int shiftY =  (diagonalSize - tileHeight) * y;
+                    points.add(new Point(pointx0 + center.x + shiftX, pointy0 + center.y + shiftY));
+                    points.add(new Point(pointx1 + center.x + shiftX, pointy1 + center.y + shiftY));
+                    points.add(new Point(pointx2 + center.x + shiftX, pointy2 + center.y + shiftY));
+                    points.add(new Point(pointx3 + center.x + shiftX, pointy3 + center.y + shiftY));
+                    Tile tile = this.getIntersection(surface, new Tile(points));
+                    if (tile != null) {
+                        tiles.add(tile);
+                    }
+                }
+            }
+            int diagonalSize = (int)Math.sqrt(Math.pow(tileWidth, 2) + Math.pow(tileHeight, 2));
+            minPointY = minPointY -  diagonalSize / 2 - surface.getJointSize() / 2;
+            minPointX = minPointX - diagonalSize / 2 - surface.getJointSize() / 2;
+            for (int y = 0; y < nbYTiles; y++) {
+                for (int x = 0; x < nbXTiles; x++) {
+                    ArrayList points = new ArrayList<Point>();
+                    Point center = new Point(minPointX + x * tileWidth + (tileWidth / 2) + (surface.getJointSize() * x), minPointY + y * tileHeight + (tileHeight / 2) + (y*surface.getJointSize()));
+                    int Xpoint0 = x * tileWidth + minPointX + (surface.getJointSize() * x) - center.x;
+                    int Ypoint0 = y * tileHeight + minPointY + (y*surface.getJointSize())  - center.y;
+                    int Xpoint1 = (x + 1) * tileWidth + minPointX + (surface.getJointSize() * x) - center.x;
+                    int Ypoint1 = y * tileHeight + minPointY + (y*surface.getJointSize())  - center.y;
+                    int Xpoint2 = (x + 1) * tileWidth + minPointX + (x*surface.getJointSize()) - center.x;
+                    int Ypoint2 = (y + 1) * tileHeight + minPointY + (y*surface.getJointSize()) - center.y;
+                    int Xpoint3 = x * tileWidth + minPointX + (x*surface.getJointSize()) - center.x;
+                    int Ypoint3 = (y + 1) * tileHeight + minPointY + (y*surface.getJointSize()) - center.y;
+                    int angle = -45;
+                    int pointx0 = (int)(Xpoint0 * Math.cos(Math.toRadians(angle)) - Ypoint0 * Math.sin(Math.toRadians(angle)));
+                    int pointy0 = (int)(Xpoint0 * Math.sin(Math.toRadians(angle)) + Ypoint0 * Math.cos(Math.toRadians(angle)));
+                    int pointx1 = (int)(Xpoint1 * Math.cos(Math.toRadians(angle)) - Ypoint1 * Math.sin(Math.toRadians(angle)));
+                    int pointy1 = (int)(Xpoint1 * Math.sin(Math.toRadians(angle)) + Ypoint1 * Math.cos(Math.toRadians(angle)));
+                    int pointx2 = (int)(Xpoint2 * Math.cos(Math.toRadians(angle)) - Ypoint2 * Math.sin(Math.toRadians(angle)));
+                    int pointy2 = (int)(Xpoint2 * Math.sin(Math.toRadians(angle)) + Ypoint2 * Math.cos(Math.toRadians(angle)));
+                    int pointx3 = (int)(Xpoint3 * Math.cos(Math.toRadians(angle)) - Ypoint3 * Math.sin(Math.toRadians(angle)));
+                    int pointy3 = (int)(Xpoint3 * Math.sin(Math.toRadians(angle)) + Ypoint3 * Math.cos(Math.toRadians(angle)));
+                    int shiftX =  (diagonalSize - tileWidth) * x;
+                    int shiftY =  (diagonalSize - tileHeight) * y;
+                    points.add(new Point(pointx0 + center.x + shiftX, pointy0 + center.y + shiftY));
+                    points.add(new Point(pointx1 + center.x + shiftX, pointy1 + center.y + shiftY));
+                    points.add(new Point(pointx2 + center.x + shiftX, pointy2 + center.y + shiftY));
+                    points.add(new Point(pointx3 + center.x + shiftX, pointy3 + center.y + shiftY));
+                    Tile tile = this.getIntersection(surface, new Tile(points));
+                    if (tile != null) {
+                        tiles.add(tile);
                     }
                 }
             }
